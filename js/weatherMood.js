3b7e42a02fbdfd79c0e6bd3e01e4dfb733077733
Camzouz <camille.confolens@gmail.com>
2017-03-31 01:03:14 +0200

    commit fichiers modifiés 2

--- js/weatherMood.js
+++ js/weatherMood.js
@@ -5,19 +5,5 @@
  */
 angular.module('weatherMoodApp', [
     "weatherMood.components",
-    // "weatherMood.services"
+    "weatherMood.services"
 ])
-
-// .config(function ($routeProvider, $locationProvider) {
-//     $routeProvider
-//         .when('/Book/:bookId', {
-//             templateUrl: 'user.html',
-//             controller: 'userCtrl',
-//         })
-//         .when('/Book/:bookId/ch/:chapterId', {
-//             templateUrl: 'chapter.html',
-//             controller: 'ChapterController'
-//         });
-//     
-//     $locationProvider.html5Mode(true);
-// });
