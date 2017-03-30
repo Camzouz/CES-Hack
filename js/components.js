eed813c20c323c544f130234ffffbda2b1ceaf29
Camzouz <camille.confolens@gmail.com>
2017-03-30 22:49:59 +0200

    commit fichiers

--- js/components.js
+++ js/components.js
@@ -4,51 +4,44 @@ const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const LANG = '&lang=fr';
 const UNIT = '&units=metric';
 const API_KEY = '&APPID=1bb8b9643fcf40f10d845bc78c254b76';
+var app = angular.module("weatherMood.components", []);
+app.component("userView", {
 
-/**
- * The weatherMood components
- */
-angular.module("weatherMood.components", [])
+    templateUrl: '/user.html',
 
-    .component("userView", {
+    bindings: {
+        city: '@',
+    },
 
-        templateUrl: '/user.html',
+    controller: ['$http',
 
-        bindings: {
-            city: '@',
-        },
+        function ($http) {
 
-        controller: ['$http',
+            this.model = {
+                city: '',
+                weather: '',
+                iconUrl: '',
+                temp: ''
+            };
 
-            function ($http) {
-
-                this.model = {
-                    city: '',
-                    weather: '',
-                    iconUrl: '',
-                    temp: ''
-                };
-
-                this.fetchWeather = () => {
-                    $http({
-                        method: 'GET',
-                        url: API_URL + this.model.city + LANG + UNIT + API_KEY
-                    }).then((response) => {
-                        console.log(response);
-                        this.model.weather = response.data.weather[0].description;
-                        this.model.iconUrl = 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png';
-                        this.model.temp = response.data.main.temp;
-                    });
-
-                    this.displayWeather(this.model.weather)
-                }
-
-                this.displayWeather = (param) => {
-
-                }
+            this.fetchWeather = () => {
+                $http({
+                    method: 'GET',
+                    url: API_URL + this.model.city + LANG + UNIT + API_KEY
+                }).then((response) => {
+                    console.log(response);
+                    this.model.weather = response.data.weather[0].description;
+                    this.model.iconUrl = 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png';
+                    this.model.temp = response.data.main.temp;
+                });
             }
-        ]
-    })
+
+            this.displayWeather = (param) => {
+
+            }
+        }
+    ]
+})
 
 // .component("historyView", {
 
@@ -63,19 +56,40 @@ angular.module("weatherMood.components", [])
 //     controller: ['historyCtrl', function (historyCtrl) {}]
 // })
 
-// .component("channelView", {
+app.component("channelView", {
 
-//     templateUrl: '/channel.html',
+    templateUrl: '/channel.html',
 
-//     bindings: {
-//         list: '<',
-//         query: '@',
-//         newItem: '@',
-//         allChecked: '<'
-//     },
+    controller: ['$scope', function ($scope) {
 
-//     controller: ['channelCtrl', function (channelCtrl) {}]
-// })
+        function onPlayerLoaded(id) {
+            DZ.player.playPlaylist(id)
+        };
+        window.dzAsyncInit = function () {
+            DZ.init({
+                appId: '8',
+                channelUrl: 'http://localhost:8080/channel.html',
+                player: {
+                    container: 'player',
+                    playlist: true,
+                    width: 650,
+                    height: 300,
+                    onload: onPlayerLoaded
+                }
+            })
+
+                (function () {
+                    var e = document.createElement('script');
+                    e.src = 'http://e-cdn-files.deezer.com/js/min/dz.js';
+                    e.async = true;
+                    document.getElementById('dz-root').appendChild(e);
+                });
+        }
+   
+    }]
+})
+
+
 
 // .component("weatherView", {
 
